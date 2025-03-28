30dfc4ba1bb2f17ac14ad216be76b6ef484a6c2a
Gabriel Persike <50594940+Gabriel-Persike@users.noreply.github.com>
2025-03-28 17:59:48 -0300

    Inclusão do relatório de atividades

---
+++ Widget Atividades Compras/datasets/ListaAtividadesDoComprasNoFluig.js
@@ -0,0 +1,188 @@
+function createDataset(fields, constraints, sortFields) {
+    try {
+        if (constraints) {
+            var [Operacao, Ano, Mes, Dia] = ExtraiConstraints(constraints);
+            var myQuery = null;
+
+            if (Operacao == "ChamadosResolvidosPorCategoria") {
+                myQuery = MontaQueryChamadosResolvidosPorCategoria(Ano, Mes, Dia);
+            }
+           
+
+            return executaQueryNoFluig(myQuery);
+        }
+    } catch (error) {
+        log.info("Erro ao executar ListaAtividadesDaControladoria: " + error);
+        var newDataset = DatasetBuilder.newDataset();
+        newDataset.addColumn("coluna");
+        newDataset.addRow(["deu erro! "]);
+        newDataset.addRow([error]);
+    }
+}
+
+function ExtraiConstraints(constraints) {
+    var Operacao = null;
+    var Mes = null;
+    var Ano = null;
+    var Dia = null;
+
+    for (i = 0; i < constraints.length; i++) {
+        if (constraints[i].fieldName == "Operacao") {
+            Operacao = constraints[i].initialValue;
+
+        } else if (constraints[i].fieldName == "Ano") {
+            Ano = constraints[i].initialValue;
+        }
+        else if (constraints[i].fieldName == "Mes") {
+            Mes = constraints[i].initialValue;
+        }
+        else if (constraints[i].fieldName == "Dia") {
+            Dia = constraints[i].initialValue;
+        }
+    }
+
+    if (Operacao && Mes) {
+        return [Operacao, Ano, Mes, Dia];
+    }
+    else {
+        throw "Constraints não informadas.";
+    }
+}
+
+function MontaQueryChamadosResolvidosPorCategoria(Ano, Mes, Dia) {
+    var DataInicio = Ano + "-" + Mes;
+
+    var DataFim = null;
+    if (Mes == 12) {
+        DataFim = (parseInt(Ano) + 1) + "-01";
+    }
+    else {
+        Mes = parseInt(Mes) + 1;
+        if (Mes < 10 ) {
+            Mes = "0" + Mes
+        }
+        DataFim = Ano + "-" +Mes;
+    }
+
+
+    var myQuery =
+        "SELECT\
+            COUNT(PROCES_WORKFLOW.NUM_PROCES) as QNTD,\
+            COUNT(CASE WHEN PROCES_WORKFLOW.END_DATE like '" + DataInicio + "-" + Dia + "%' THEN 1 END) as 'QNTDHOJE',\
+            MLSUPORTECOMPRAS.selectMotivo\
+        FROM PROCES_WORKFLOW\
+            INNER JOIN ML00139978 MLSUPORTECOMPRAS ON MLSUPORTECOMPRAS.documentid = PROCES_WORKFLOW.NR_DOCUMENTO_CARD\
+            INNER JOIN TAR_PROCES ON PROCES_WORKFLOW.NUM_PROCES = TAR_PROCES.NUM_PROCES AND TAR_PROCES.IDI_STATUS = 2 AND TAR_PROCES.NUM_SEQ_ESCOLHID = 6\
+        WHERE\
+            PROCES_WORKFLOW.END_DATE >= '" + DataInicio + "-01'\
+            AND PROCES_WORKFLOW.END_DATE < '" + DataFim + "-01'\
+            AND  MLSUPORTECOMPRAS.atendimento != ''\
+        GROUP BY MLSUPORTECOMPRAS.selectMotivo\
+        ORDER BY QNTD DESC";
+
+    return myQuery;
+}
+
+
+
+
+function executaQueryNoFluig(query) {
+    var newDataset = DatasetBuilder.newDataset();
+    var dataSource = "/jdbc/FluigDSRO";
+    var ic = new javax.naming.InitialContext();
+    var ds = ic.lookup(dataSource);
+    var created = false;
+
+    try {
+        var conn = ds.getConnection();
+        var stmt = conn.createStatement();
+        var rs = stmt.executeQuery(query);
+        var columnCount = rs.getMetaData().getColumnCount();
+
+        while (rs.next()) {
+            if (!created) {
+                for (var i = 1; i <= columnCount; i++) {
+                    newDataset.addColumn(rs.getMetaData().getColumnName(i));
+                }
+                created = true;
+            }
+            var Arr = new Array();
+            for (var i = 1; i <= columnCount; i++) {
+                var obj = rs.getObject(rs.getMetaData().getColumnName(i));
+                if (null != obj) {
+                    Arr[i - 1] = rs.getObject(rs.getMetaData().getColumnName(i)).toString();
+                } else {
+                    Arr[i - 1] = "   -   ";
+                }
+            }
+
+            newDataset.addRow(Arr);
+        }
+    } catch (e) {
+        log.error("ERRO==============> " + e.message);
+        newDataset.addColumn("coluna");
+        newDataset.addRow(["deu erro! "]);
+        newDataset.addRow([e.message]);
+        newDataset.addRow([query]);
+
+    } finally {
+        if (stmt != null) {
+            stmt.close();
+        }
+        if (conn != null) {
+            conn.close();
+        }
+    }
+
+    return newDataset;
+}
+function executaQueryNoCastilhoCustom(query) {
+    var newDataset = DatasetBuilder.newDataset();
+    var dataSource = "/jdbc/CastilhoCustom";
+    var ic = new javax.naming.InitialContext();
+    var ds = ic.lookup(dataSource);
+    var created = false;
+
+    try {
+        var conn = ds.getConnection();
+        var stmt = conn.createStatement();
+        var rs = stmt.executeQuery(query);
+        var columnCount = rs.getMetaData().getColumnCount();
+
+        while (rs.next()) {
+            if (!created) {
+                for (var i = 1; i <= columnCount; i++) {
+                    newDataset.addColumn(rs.getMetaData().getColumnName(i));
+                }
+                created = true;
+            }
+            var Arr = new Array();
+            for (var i = 1; i <= columnCount; i++) {
+                var obj = rs.getObject(rs.getMetaData().getColumnName(i));
+                if (null != obj) {
+                    Arr[i - 1] = rs.getObject(rs.getMetaData().getColumnName(i)).toString();
+                } else {
+                    Arr[i - 1] = "   -   ";
+                }
+            }
+
+            newDataset.addRow(Arr);
+        }
+    } catch (e) {
+        log.error("ERRO==============> " + e.message);
+        newDataset.addColumn("coluna");
+        newDataset.addRow(["deu erro! "]);
+        newDataset.addRow([e.message]);
+        newDataset.addRow([query]);
+
+    } finally {
+        if (stmt != null) {
+            stmt.close();
+        }
+        if (conn != null) {
+            conn.close();
+        }
+    }
+
+    return newDataset;
+}
--- Widget Atividades Compras/wcm/widget/Widget_Atividades_Compras/src/main/resources/view.ftl
+++ Widget Atividades Compras/wcm/widget/Widget_Atividades_Compras/src/main/resources/view.ftl
@@ -1,4 +1,88 @@
 <div id="MyWidget_${instanceId}" class="super-widget wcm-widget-class fluig-style-guide" data-params="MyWidget.instance()">
+    <script type="text/javascript" src="/webdesk/vcXMLRPC.js"></script>
+    <h3>Atividades Compras</h3>
 
+    <div class="panel panel-primary" id="panelFiltros" style="margin-bottom: 4px">
+        <div class="panel-heading" style="padding: 8px 12px">
+            <h3 class="panel-title">Filtros</h3>
+        </div>
+        <div class="panel-body" style="padding: 8px 12px">
+            <div id="FiltrosData" class="row">
+                <div class="col-md-6">
+                    <label for="MesFiltro">Mês:</label>
+                    <select name="MesFiltro" id="MesFiltro" class="form-control">
+                        <option value="1">Janeiro</option>
+                        <option value="2">Fevereiro</option>
+                        <option value="3">Março</option>
+                        <option value="4">Abril</option>
+                        <option value="5">Maio</option>
+                        <option value="6">Junho</option>
+                        <option value="7">Julho</option>
+                        <option value="8">Agosto</option>
+                        <option value="9">Setembro</option>
+                        <option value="10">Outubro</option>
+                        <option value="11">Novembro</option>
+                        <option value="12">Dezembro</option>
+                    </select>
+                </div>
+                <div class="col-md-6">
+                    <label for="AnoFiltro">Ano:</label>
+                    <select name="AnoFiltro" id="AnoFiltro" class="form-control">
+                        <option value="2024">2024</option>
+                        <option value="2023">2023</option>
+                        <option value="2022">2022</option>
+                        <option value="2021">2021</option>
+                        <option value="2020">2020</option>
+                        <option value="2019">2019</option>
+                        <option value="2018">2018</option>
+                    </select>
+                </div>
+            </div>
+            <br />
+            <div id="FiltroRelatorio" class="row">
+                <div class="col-md-12">
+                    <label for="Relatorio">Relatório:</label>
+                    <select name="Relatorio" id="Relatorio" class="form-control">
+                        <option value="Todas as Atividades">Todas as Atividades</option>
+                    </select>
+                </div>
+            </div>
+            <br />
+            <div id="divAutoRefresh" class="row">
+                <div class="col-md-12">
+                    <label for="autoRefresh">Auto-refresh:</label>
+                    <select name="autoRefresh" id="autoRefresh" class="form-control">
+                        <option value="Desativado">Desativado</option>
+                        <option value="1">1 Minutos</option>
+                        <option value="5" selected>5 Minutos</option>
+                        <option value="10">10 Minutos</option>
+                    </select>
+                </div>
+            </div>
+            <br />
+            <div id="divExportarExcel" class="row">
+                <div class="col-md-12" style="text-align: right">
+                    <button class="btn btn-primary" id="btnExportarDados">
+                        <i class="flaticon flaticon-export icon-sm" aria-hidden="true"></i>
+                        Exportar
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="divListaAtividades" style="overflow: auto">
+        <div id="TodasAtividades">
+            <table class="table table-bordered table-striped table-condensed">
+                <thead>
+                    <tr>
+                        <th>Categoria</th>
+                        <th>Quantidade</th>
+                        <th class="ThHoje">Hoje</th>
+                    </tr>
+                </thead>
+                <tbody id="tbodyTodasAtividades"></tbody>
+            </table>
+        </div>
+    </div>
 </div>
-
